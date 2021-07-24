1bae2119f3b2e4100616dbd3c33ac510caf8a0bf
leehyemmin <ham6482@naver.com>
2021-07-24 13:21:45 +0900

    Stream

---
+++ demo02/src/ex07/StreamEx5.java
@@ -0,0 +1,61 @@
+package ex07;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StreamEx5 {
+	public static void main(String[] args) {
+		Stream<Student> studentStream = Stream.of(
+					new Student("이자바", 3, 300),
+					new Student("김자바", 1, 200),
+					new Student("안자바", 2, 100),
+					new Student("박자바", 2, 150),
+					new Student("소자바", 1, 200),
+					new Student("나자바", 3, 290),
+					new Student("감자바", 3, 180)
+				);
+		
+//		studentStream.sorted(Comparator.comparing(Student::getBan)  // 1. 반별 정렬
+				studentStream.sorted(Comparator.comparing((Student s) -> s.getBan())  // 위를 람다식으로
+				.thenComparing(Comparator.naturalOrder())) 	// 2. 기본 정렬
+				.forEach(System.out::println);
+				
+	}
+
+}
+
+class Student implements Comparable<Student>{
+	String name;
+	int ban;
+	int totalScore;
+	
+	Student(String name, int ban, int totalScore){
+		this.name = name;
+		this.ban = ban;
+		this.totalScore = totalScore;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[%s, %d, %d]", name, ban, totalScore);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBan() {
+		return ban;
+	}
+
+	public int getTotalScore() {
+		return totalScore;
+	}
+	
+// 총점 내림차순을 기본 정렬로 한다.
+	public int compareTo(Student s) {
+		return s.totalScore - this.totalScore;
+	}
+	
+	
+}

d9b62881aec8a68f2673393d29aa9064a379d927
leehyemmin <ham6482@naver.com>
2021-10-15 00:48:35 +0900

    for

---
+++ demo02/src/review/Ex3_1.java
@@ -0,0 +1,17 @@
+package review;
+
+import java.util.*;
+
+public class Ex3_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N;
+		N = sc.nextInt();
+		
+		for(int i = 1;i<10;i++) {
+			System.out.println(N + "*" + i + " = " + (N*i));
+		}
+	}
+
+}
---
+++ demo02/src/review/Ex3_2.java
@@ -0,0 +1,22 @@
+package review;
+
+import java.util.Scanner;
+
+public class Ex3_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+
+		
+		for(int i=0;i<T;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println(a + b);
+		}
+
+	}
+
+}
